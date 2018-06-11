e2159d933e076aef28060ba2ba08cb3abb18016b
Simon Hellinger <simon.hellinger@cgm.com>
2018-06-11 16:41:12 +0200

    Added redux dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,33 @@
+import { createStore } from 'redux'
+
 import TaskCollection from './TaskCollection'
 
 new TaskCollection([
     'Go to store',
     'Eat cake'
 ]).dump();
+
+// React testing ground
+
+// actions
+const ADD_TODO = 'ADD_TODO';
+
+function addTodo(text: string) {
+	return {
+		type: ADD_TODO,
+		text
+	}
+}
+
+// state
+const initialState = {
+	todos: []
+}
+
+// reducer
+function todoApp(state = initialState) {
+	return state;
+}
+
+// store
+const store = createStore(todoApp);
